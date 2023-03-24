745bb212cef5027fb4e7f0b6bed37552595ccf08
brakdag <brakdag@gmail.com>
2023-03-24 11:58:46 -0300

    casi terminado

--- test/test.js
+++ test/test.js
@@ -1,15 +1,46 @@
 const gem = require("../lib/index") 
-const g = new gem(process.env.USER,process.env.PASS);
+const fcm = require("../lib/fcm")
+const cred = require("../lib/credentials")
 
-describe("Testeando",function(done2){
+let credentials = new cred().get()
+
+const g = new gem(credentials.gem.user,credentials.gem.pass);
+
+describe("Testeando",function(){
 it("Login",function (done){ 
         g.login(l=>{
             g.llamado(x=>{
-                console.log(x.length)
                 console.log(x.map(a=>[a.id*1,`${a.fecha_llamado} - ${a.lugar_trabajo}-${a.direccion}-${a.localidad}`]))
                 done()
             })
         })
 })
+
+it("FCM",function(done){
+    let  a = new fcm();
+    a.send("prueba","gustavo",(x)=>{
+        console.log(x)
+        done();
+    });
+     
+});
+
+
+it.only("LLamado+notif",function (done){ 
+    g.login(l=>{
+        g.llamado(x=>{
+            let  a = new fcm();
+            for(let g of x){
+            a.send(g.lugar_trabajo,`${g.fecha_llamado} - ${g.lugar_trabajo}-${g.direccion}-${g.localidad}`,(y)=>{done()})        
+            }
+        });
+    });
+})
+
+
+
+
+
+
 
 })
