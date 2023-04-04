c5b3d3769c8b709713600b4bd63df04f7f441951
Gustavo Ferreyra <brakdag@gmail.com>
2023-04-04 05:58:56 +0000

    arreglando para docker

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const cred = require("../lib/credentials")
-const monitor = require("../lib/monitor")
+const cred = require("./lib/credentials")
+const monitor = require("./lib/monitor")
 
 let credentials = new cred().get()
 let mon = new monitor(credentials.gem.user,credentials.gem.pass)
