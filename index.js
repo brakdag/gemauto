44cfff0e5f2e1f874c95d82391183d4f00b01e35
root <brakdag@gmail.com>
2023-04-10 07:26:33 -0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const cred = require("./lib/credentials")
 const monitor = require("./lib/monitor")
+const express = require('express')
 
 let credentials = new cred().get()
 let mon = new monitor(credentials.gem.user,credentials.gem.pass)
+  
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Servidor gemauto funcionando')
+})
+
+app.listen(8080, () => {
+  console.log('Servidor gemauto en 8000')
+})
+
+
